23ff88d4a233e6bcd44b12893e6ccd97960d7949
Nikolaos Karaolidis <nick@karaolidis.com>
2024-05-05 13:49:26 +0100

    Add multiple-file processing
    Signed-off-by: Nikolaos Karaolidis <nick@karaolidis.com>

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,21 @@ use log::{error, info, warn};
 use pathsearch::find_executable_in_path;
 use std::{
     fs,
-    path::Path,
-    process::{Command, Stdio},
+    path::{Path, PathBuf},
+    process::Command,
+    sync::Arc,
     time::Duration,
 };
-use wait_timeout::ChildExt;
+use tokio::{spawn, sync::Semaphore, task::JoinHandle};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, author)]
 struct Args {
-    /// Path to the Java file/folder to be analyzed.
-    /// If it's a folder, the file must be named either Main.java or the same as the folder.
-    file_path: String,
+    /// Path(s) to the Java file(s)/folder(s) to be analyzed.
+    /// If a path is a folder, the file must be named either Main.java or the same as the folder.
+    /// Entrypoint can be specified with the format path:entrypoint.
+    /// If no entrypoint is provided, the default entrypoint is used.
+    file_paths: Vec<String>,
 
     /// Entrypoint function to be analyzed
     #[arg(short, long, default_value = "test")]
@@ -42,11 +45,16 @@ struct Args {
 
     /// Execution timeout in seconds
     #[arg(short, long, default_value = "60")]
-    timeout: u64,
+    timeout: usize,
+
+    /// Maximum number of threads to use
+    #[arg(short = 'n', long, default_value = "1")]
+    threads: usize,
 }
 
+#[tokio::main]
 #[allow(clippy::too_many_lines)]
-fn main() {
+async fn main() {
     log4rs::init_config(
         log4rs::Config::builder()
             .appender(
@@ -72,105 +80,137 @@ fn main() {
 
     let args = Args::parse();
 
-    let mut file_path = canonicalize(Path::new(&args.file_path))
-        .expect("Failed to canonicalize file path, ensure the file exists");
-
-    if let Ok(p) = file_path.strip_prefix(r"\\?\") {
-        file_path = p.to_path_buf();
-    }
-
-    if file_path.is_dir() {
-        let folder_name = file_path.file_name().unwrap().to_str().unwrap();
-
-        let main_file = file_path.join("Main.java");
-        let folder_file = file_path.join(format!("{folder_name}.java"));
-
-        if main_file.exists() {
-            file_path = main_file;
-        } else if folder_file.exists() {
-            file_path = folder_file;
-        } else {
-            panic!("Failed to find Main.java or {folder_name}.java in the folder");
-        }
-    }
-
-    let mut javac_path = canonicalize(Path::new(&args.javac_path)
+    let javac_path = canonicalize(Path::new(&args.javac_path)
         )
         .unwrap_or_else(|_| {
             find_executable_in_path(&args.javac_path).expect("Failed to find javac, ensure that the Java compiler is installed and accessible via the path in -c (--javac-path)")
         });
 
-    if let Ok(p) = javac_path.strip_prefix(r"\\?\") {
-        javac_path = p.to_path_buf();
-    }
-
-    let mut jbmc_path = canonicalize(Path::new(&args.jbmc_path)
+    let jbmc_path = canonicalize(Path::new(&args.jbmc_path)
         )
         .unwrap_or_else(|_| find_executable_in_path(&args.jbmc_path).expect("Failed to find jbmc, ensure that JBMC is installed and accessible via the path in -j (--jbmc-path)"));
 
-    if let Ok(p) = jbmc_path.strip_prefix(r"\\?\") {
-        jbmc_path = p.to_path_buf();
-    }
+    let timeout = Duration::from_secs(args.timeout.try_into().unwrap());
 
-    let timeout = Duration::from_secs(args.timeout);
-
-    let mut javac_proc = Command::new(&javac_path)
+    let javac_output = Command::new(&javac_path)
         .arg("--version")
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("Failed to spawn javac --version");
+        .output()
+        .expect("Failed to run javac --version, ensure that the Java compiler is installed and accessible via the path in -c (--javac-path)");
 
-    if javac_proc.wait_timeout(timeout).unwrap().is_some() {
-        let output = javac_proc.wait_with_output().unwrap();
+    let javac_version = String::from_utf8_lossy(&javac_output.stdout)
+        .trim()
+        .to_string();
 
-        let javac_version = String::from_utf8_lossy(&output.stdout).trim().to_string();
-        info!("Java compiler version: {}", javac_version);
+    info!("Java compiler version: {}", javac_version);
 
-        if !javac_version.contains(" 17.") {
-            warn!("JBMC may not work as expected with Java versions other than 17");
-        }
-    } else {
-        javac_proc.kill().expect("Failed to kill Java compiler");
-        panic!("Java compiler took too long to respond")
+    if !javac_version.contains(" 17.") {
+        warn!("JBMC may not work as expected with Java versions other than 17");
     }
 
-    let mut jbmc_proc = Command::new(&jbmc_path)
+    let jbmc_output = Command::new(&jbmc_path)
         .arg("--version")
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .expect("Failed to run jbmc --version");
+        .output()
+        .expect("Failed to run jbmc --version, ensure that JBMC is installed and accessible via the path in -j (--jbmc-path)");
 
-    if jbmc_proc.wait_timeout(timeout).unwrap().is_some() {
-        let output = jbmc_proc.wait_with_output().unwrap();
+    let jbmc_version = String::from_utf8_lossy(&jbmc_output.stdout)
+        .trim()
+        .to_string();
 
-        let jbmc_version = String::from_utf8_lossy(&output.stdout).trim().to_string();
-        info!("JBMC version: {}", jbmc_version);
-    } else {
-        jbmc_proc.kill().expect("Failed to kill JBMC");
-        panic!("JBMC took too long to respond")
-    }
+    info!("JBMC version: {}", jbmc_version);
 
-    compile_java_class(&file_path, &javac_path, timeout);
-    let class = file_path.file_stem().unwrap().to_str().unwrap();
+    let (file_paths, entrypoints): (Vec<PathBuf>, Vec<String>) = args
+        .file_paths
+        .iter()
+        .filter_map(|fpe| {
+            let (file_path, entrypoint) = if let Some(colon) = fpe.rfind(':') {
+                let (file_path, entrypoint) = fpe.split_at(colon);
 
-    let output = run_jbmc(&file_path, &args.entrypoint, &jbmc_path, timeout);
-    let counterexamples = parse_jdmc_output(output, class, &args.entrypoint);
+                let file_path = Path::new(file_path).to_path_buf();
+                let entrypoint = &entrypoint[1..];
 
-    for (i, counterexample) in counterexamples.into_iter().enumerate() {
-        match counterexample {
-            Ok(counterexample) => {
-                let file = file_path.with_file_name(format!("{class}CE_{i}.java"));
-                let result = fs::write(&file, counterexample);
+                if file_path.exists() {
+                    (file_path, entrypoint.to_string())
+                } else {
+                    (Path::new(fpe).to_path_buf(), args.entrypoint.clone())
+                }
+            } else {
+                (Path::new(fpe).to_path_buf(), args.entrypoint.clone())
+            };
 
-                if let Err(e) = result {
-                    error!("Failed to write counterexample to file: {}", e);
+            let canonical = canonicalize(&file_path);
+
+            if let Err(e) = canonical {
+                error!("Failed to canonicalize file path {file_path:?}: {e}",);
+                return None;
+            }
+
+            let mut file_path = canonical.unwrap();
+
+            if file_path.is_dir() {
+                let folder_name = file_path.file_name().unwrap().to_str().unwrap();
+
+                let main_file = file_path.join("Main.java");
+                let folder_file = file_path.join(format!("{folder_name}.java"));
+
+                if main_file.exists() {
+                    file_path = canonicalize(&main_file).unwrap();
+                } else if folder_file.exists() {
+                    file_path = canonicalize(&folder_file).unwrap();
+                } else {
+                    error!("No Main.java or {folder_name}.java found in {file_path:?}");
+                    return None;
                 }
             }
-            Err(e) => {
-                error!("Failed to generate counterexample: {}", e);
+
+            Some((file_path, entrypoint))
+        })
+        .unzip();
+
+    let mut tasks: Vec<JoinHandle<()>> = Vec::new();
+    let semaphore = Arc::new(Semaphore::new(args.threads));
+
+    for (file_path, entrypoint) in file_paths.into_iter().zip(entrypoints) {
+        let javac_path = javac_path.clone();
+        let jbmc_path = jbmc_path.clone();
+
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
+
+        let task = spawn(async move {
+            let _permit = permit;
+
+            compile_java_class(&file_path, &javac_path, timeout).await;
+            let class = file_path.file_stem().unwrap().to_str().unwrap();
+
+            let output = run_jbmc(&file_path, &entrypoint, &jbmc_path, timeout).await;
+
+            if let Err(e) = output {
+                error!("JBMC error: {}", e);
+                return;
             }
-        }
+
+            let counterexamples = parse_jdmc_output(output.unwrap(), class, &entrypoint);
+
+            for (i, counterexample) in counterexamples.into_iter().enumerate() {
+                match counterexample {
+                    Ok(counterexample) => {
+                        let file = file_path.with_file_name(format!("{class}CE_{i}.java"));
+                        let result = fs::write(&file, counterexample);
+
+                        if let Err(e) = result {
+                            error!("Failed to write counterexample to file: {}", e);
+                        }
+                    }
+                    Err(e) => {
+                        error!("Failed to generate counterexample: {}", e);
+                    }
+                }
+            }
+        });
+
+        tasks.push(task);
+    }
+
+    for task in tasks {
+        task.await.unwrap();
     }
 }
--- src/runner.rs
+++ src/runner.rs
@@ -1,60 +1,64 @@
 use crate::jbmc::{self, Output};
 
-use std::{
-    path::Path,
-    process::{Command, Stdio},
-    time::Duration,
-};
-use wait_timeout::ChildExt;
+use log::{error, info};
+use std::{error::Error, path::Path, process::Stdio, time::Duration};
+use tokio::{process::Command, time::timeout};
 
-pub fn compile_java_class(file: &Path, javac_path: &Path, timeout: Duration) {
+pub async fn compile_java_class(file: &Path, javac_path: &Path, duration: Duration) {
     let mut proc = Command::new(javac_path)
         .arg(file)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
+        .kill_on_drop(true)
         .spawn()
         .expect("Failed to spawn Java compiler");
 
-    if let Some(status) = proc.wait_timeout(timeout).unwrap() {
-        let output = proc.wait_with_output().unwrap();
-
-        assert!(
-            status.success(),
-            "Failed to compile Java file: {}",
-            String::from_utf8_lossy(&output.stderr)
-        );
-    } else {
-        proc.kill().expect("Failed to kill Java compiler");
-        panic!("Java compiler timed out");
+    match timeout(duration, proc.wait()).await {
+        Ok(Ok(status)) if status.success() => info!("Finished compiling {file:?}"),
+        Ok(Ok(_)) => unreachable!(),
+        Ok(Err(e)) => error!("Java compiler error: {e}"),
+        Err(_) => error!("Java compiler timed out"),
     }
 }
 
-pub fn run_jbmc(file: &Path, entrypoint: &str, jbmc_path: &Path, timeout: Duration) -> Vec<Output> {
+pub async fn run_jbmc(
+    file: &Path,
+    entrypoint: &str,
+    jbmc_path: &Path,
+    duration: Duration,
+) -> Result<Vec<Output>, Box<dyn Error>> {
     let stem = file.file_stem().unwrap().to_str().unwrap();
 
-    let mut proc = Command::new(jbmc_path)
+    let proc = Command::new(jbmc_path)
         .arg("--json-ui")
         .arg(format!("{stem}.{entrypoint}"))
         .current_dir(file.parent().unwrap())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
+        .kill_on_drop(true)
         .spawn()
         .expect("Failed to spawn JBMC");
 
-    if proc.wait_timeout(timeout).unwrap().is_some() {
-        let output = proc.wait_with_output().unwrap();
-        let output = serde_json::from_slice::<Vec<Output>>(&output.stdout).unwrap();
+    match timeout(duration, proc.wait_with_output()).await {
+        Ok(Ok(output)) => {
+            let output = serde_json::from_slice::<Vec<Output>>(&output.stdout)?;
 
-        if let Some(e) = output.iter().find_map(|o| match o {
-            Output::Message(jbmc::Message::Error(e)) => Some(e.trim()),
-            _ => None,
-        }) {
-            panic!("JBMC error: {e}");
+            if let Some(e) = output.iter().find_map(|o| match o {
+                Output::Message(jbmc::Message::Error(e)) => Some(e.trim()),
+                _ => None,
+            }) {
+                return Err(e.into());
+            }
+
+            Ok(output)
         }
-
-        output
-    } else {
-        proc.kill().expect("Failed to kill JBMC");
-        panic!("JBMC timed out");
+        Ok(Err(e)) => {
+            error!("JBMC error: {e}");
+            Err(e.into())
+        }
+        Err(_) => {
+            error!("JBMC timed out");
+            Err("JBMC timed out".into())
+        }
     }
 }
