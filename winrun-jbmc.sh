d20d9a21d09a2677bfbbf14076096d13dd7008ea
panostrian <takistrian@gmail.com>
2024-05-06 17:45:30 +0100

    Create winrun-jbmc.sh

---
+++ winrun-jbmc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#For Windows ensure cbmc/bin is in the Environment path list.
+
+base_dir="./examples/benchmark"
+num_threads=1
+
+run_jbmc() {
+    folder="$1"
+    stdout_file="$folder/stdout.log"
+    stderr_file="$folder/stderr.log"
+    ./target/release/jbmc-counterexample -j jbmc "$folder" >"$stdout_file" 2>"$stderr_file"
+}
+
+export -f run_jbmc
+
+cleanup() {
+    echo "Cleaning up..."
+    pkill -P $$
+    exit 1
+}
+
+trap cleanup SIGINT SIGTERM
+
+find "$base_dir" -maxdepth 1 -type d -print0 | xargs -0 -P "$num_threads" -I {} bash -c 'run_jbmc "{}"'
